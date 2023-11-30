78d181289e164d3d650428e4209ae66f516fe26b
shahid1105 <shahidhasan1105@gmail.com>
2023-11-30 13:06:25 +0600

    add banner

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,7 +1,56 @@
+import image1 from "../../../../public/image1.jpeg";
+import image2 from "../../../../public/image2.jpeg";
+import image3 from "../../../../public/image3.jpeg";
+import image4 from "../../../../public/images4.jpeg";
+
 const Banner = () => {
   return (
-    <div>
-      <h1>this is banner</h1>
+    <div className="carousel w-full rounded-r-full rounded-l-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={image3} className="w-full h-64" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={image4} className="w-full h-64" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={image1} className="w-full h-64" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={image2} className="w-full h-64" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
